1f2df0ad7181d9da8531f0f8697da54b4ab5222a
plosnak2 <jakub.zaukolec@gmail.com>
2023-05-31 14:50:27 +0200

    basic setup + terminal window created

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div className='terminal'>
+        <div className='panel'>
+
+        </div>
+        <div className='terminal-body'>
+
+        </div>
+      </div>      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/data.ts
@@ -0,0 +1,30 @@
+export const myCommands = [
+    {
+        "name": "help",
+        "value": "This is Help"
+    },
+    {
+        "whoami": "help",
+        "value": "This is whoami"
+    },
+    {
+        "name": "contact",
+        "value": "This is contact"
+    },
+    {
+        "name": "clear",
+        "value": ""
+    },
+    {
+        "name": "linkedin",
+        "value": "This is linkedin"
+    },
+    {
+        "name": "github",
+        "value": "This is github"
+    },
+    {
+        "name": "",
+        "value": ""
+    },
+]
