f202cd0029606689f299038ddce363d305d8d27a
plosnak2 <jakub.zaukolec@gmail.com>
2023-05-31 23:25:21 +0200

    added toast and functionality for green button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Terminal } from './components/Terminal';
 import { useRef, useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   // ref for focusing input
@@ -16,11 +17,15 @@ function App() {
 
   return (
     <div>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
       <div className={`terminal + ${closed ? 'hidden-terminal' : ''}`} onClick={handleClick}>
         <div className='panel'>
           <div className='circle' onClick={() => {setClosed(true)}}></div>
           <div className='circle' onClick={() => {setMinimalized(!minimalized)}}></div>
-          <div className='circle'></div>
+          <div className='circle' onClick={() => {toast.success('Welcome to my terminal app!')}}></div>
         </div>
         <div className={`terminal-body + ${minimalized ? 'hidden-terminal' : ''}`}>
           <Terminal refInput={refInput}/>
